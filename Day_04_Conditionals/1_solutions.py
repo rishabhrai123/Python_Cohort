cec464dbdecfc9ce5e252a5ca795cc9020c920df
Rishabh Rai <rai735779@gmail.com>
2026-07-16 14:16:25 +0530

    solution_1

---
+++ Day_04_Conditionals/1_solutions.py
@@ -0,0 +1,12 @@
+age=input("Provide  me an age: ")
+
+age_in_int=int(age)
+
+if (age<13):
+    print("Child")
+elif(age<20):
+    print("Teenager")
+elif(age<60):
+    print("Adult")
+else:
+    print("Senior")
